e25ed6d5c9629ee64c0dda0079cd47c68342c447
Amith Krishnan <amithkrishnan20@gmail.com>
2023-07-10 08:28:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res) {
+    
+    var today = new Date();
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req, res) {
+    item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("server started on port 3000");
+})
