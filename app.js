42e940257aeda847c727df6a5f002b5119208693
Amith Krishnan <amithkrishnan20@gmail.com>
2023-07-11 23:39:12 +0530

    header and footer seperated by ejs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 var items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
@@ -25,15 +26,23 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-    item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    let item = req.body.newItem;
+    if (req.body.list = "work") {
+        workItems.push(item);
+    res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");  
+    }
+    
 });
 
 
 app.get("/work", function(req, res) {
-    res.render()
-})
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+});
+
+
 
 
 app.listen(process.env.PORT || 3000, function() {
